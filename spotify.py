61b1ba145552ae8501957f20f8c5edf46687a44f
Liam McLoughlin <hexxeh@hexxeh.net>
2012-11-18 01:19:08 +0000

    Improve mget requests and reply handling

--- spotify.py
+++ spotify.py
@@ -75,18 +75,31 @@ class SpotifyUtil():
 		return uri.split(":")[1]
 
 	@staticmethod
-	def metadata_resp_to_obj(metadata_type, resp):
-		if metadata_type == "album":
-			obj = metadata_pb2.Album()
-		elif metadata_type == "track":
-			obj = metadata_pb2.Track()
-		elif metadata_type == "artist":
-			obj = metadata_pb2.Artist()
+	def metadata_resp_to_obj(resp):
+		header = mercury_pb2.MercuryReply()
+		header.ParseFromString(base64.decodestring(resp[0]))
+
+		if header.status_message == "vnd.spotify/mercury-mget-reply":
+			mget_reply = mercury_pb2.MercuryMultiGetReply()
+			mget_reply.ParseFromString(base64.decodestring(resp[1]))
+			items = []
+			for reply in mget_reply.reply:
+				items.append(SpotifyUtil.parse_metadata_item(reply.content_type, reply.body))
+			return items
 		else:
-			Logging.error("invalid metadata_type given")
+			return SpotifyUtil.parse_metadata_item(header.status_message, base64.decodestring(resp[1]))
+
+	@staticmethod
+	def parse_metadata_item(content_type, body):
+		if content_type == "vnd.spotify/metadata-album":
+			obj = metadata_pb2.Album()
+		elif content_type == "vnd.spotify/metadata-track":
+			obj = metadata_pb2.Track()
+		else:
+			Logging.error("Unrecognised metadata type " + content_type)
 			return False
 
-		obj.ParseFromString(base64.decodestring(resp[1]))
+		obj.ParseFromString(body)
 		return obj
 
 class SpotifyAPI():
@@ -153,12 +166,37 @@ class SpotifyAPI():
 		args = [codec, id]
 		self.send_command("sp/track_uri", args, callback)
 
-	def metadata_request(self, metadata_type, id, callback):
-		mercury_request = mercury_pb2.MercuryRequest()
-		mercury_request.body = "GET"
-		mercury_request.uri = "hm://metadata/"+metadata_type+"/"+id
-		req = base64.encodestring(mercury_request.SerializeToString())
-		args = [0, req]
+	def metadata_request(self, uris, callback):
+		mercury_requests = mercury_pb2.MercuryMultiGetRequest()
+
+		if type(uris) != list:
+			uris = [uris]
+
+		for uri in uris:
+			uri_type = SpotifyUtil.get_uri_type(uri)
+			id = SpotifyUtil.uri2id(uri)
+
+			mercury_request = mercury_pb2.MercuryRequest()
+			mercury_request.body = "GET"
+			mercury_request.uri = "hm://metadata/"+uri_type+"/"+id
+
+			mercury_requests.request.extend([mercury_request])
+
+		args = [0]
+
+		if len(mercury_requests.request) == 1:
+			req = base64.encodestring(mercury_requests.request[0].SerializeToString())
+			args.append(req)
+		else:
+			header = mercury_pb2.MercuryRequest()
+			header.body = "GET"
+			header.uri = "hm://metadata/"+SpotifyUtil.get_uri_type(uris[0])+"s"
+			header.content_type = "vnd.spotify/mercury-mget-request"
+
+			header_str = base64.encodestring(header.SerializeToString())
+			req = base64.encodestring(mercury_requests.SerializeToString())
+			args.extend([header_str, req])
+
 		self.send_command("sp/hm_b64", args, callback)
 
 	def playlist_request(self, playlist_id, fromnum, num, callback):
@@ -187,11 +225,11 @@ class SpotifyAPI():
 
 	def send_string(self, msg):
 		msg_enc = json.dumps(msg, separators=(',',':'))
-		Logging.debug("sent " + msg_enc)
+		#Logging.debug("sent " + msg_enc)
 		self.ws.send(msg_enc)
 
 	def recv_packet(self, msg):
-		Logging.debug("recv " + str(msg))
+		#Logging.debug("recv " + str(msg))
 		packet = json.loads(str(msg))
 		if "error" in packet:
 			self.handle_error(packet)
@@ -206,16 +244,41 @@ class SpotifyAPI():
 			else:
 				Logging.notice("Unhandled command response with id " + str(pid))
 
+	def work_callback(self, sp, resp):
+		Logging.debug("Got ack for message reply")
+
 	def handle_message(self, msg):
 		cmd = msg[0]
 		if len(msg) > 1:
 			payload = msg[1]
 		if cmd == "do_work":
 			Logging.debug("Got do_work message, payload: "+payload)
-			self.send_command("sp/work_done", ["v2"])
+			self.send_command("sp/work_done", ["v1"], self.work_callback)
 
 	def handle_error(self, err):
-		Logging.error(str(err))
+		if len(err) < 2:
+			Logging.error("Unknown error "+str(err))
+
+		major = err["error"][0]
+		minor = err["error"][1]
+
+		major_err = {
+			12: "Track error",
+			13: "Hermes error",
+			14: "Hermes service error",
+		}
+
+		minor_err = {
+			1: "failed to send to backend",
+			8: "rate limited",
+			408: "timeout",
+			429: "too many requests",
+		}
+
+		if minor == 0:
+			Logging.error(major_err[major])
+		else:
+			Logging.error(major_err[major] + " - " + minor_err[minor])
 
 	def connect(self):
 		if self.settings == None:
@@ -237,36 +300,45 @@ class SpotifyAPI():
 def track_uri_callback(sp, result):
 	print "URL: "+result["uri"]
 
+def multi_track_metadata_callback(sp, result):
+	tracks = SpotifyUtil.metadata_resp_to_obj(result)
+	for track in tracks:
+		print track.name
+
 def track_metadata_callback(sp, result):
-	print result
-	track = SpotifyUtil.metadata_resp_to_obj("track", result)
+	track = SpotifyUtil.metadata_resp_to_obj(result)
 	print track.name
 
 def album_metadata_callback(sp, result):
-	album = SpotifyUtil.metadata_resp_to_obj("album", result)
+	album = SpotifyUtil.metadata_resp_to_obj(result)
 	print album.name+"\n"
+	uris = []
 	for track in album.disc[0].track:
-		sp.metadata_request("track", SpotifyUtil.gid2id(track.gid), track_metadata_callback)
+		uris.append(SpotifyUtil.id2uri("track", SpotifyUtil.gid2id(track.gid)))
 		#sp.track_uri(SpotifyUtil.gid2id(track.gid), "mp3160", track_uri_callback)
+	sp.metadata_request(uris, multi_track_metadata_callback)
 
 def playlist_callback(sp, result):
 	obj = playlist4changes_pb2.ListDump()
 	res = base64.decodestring(result[1])
 	obj.ParseFromString(res)
 	print obj.attributes.name+"\n"
+	uris = []
 	for track in obj.contents.items:
 		if SpotifyUtil.get_uri_type(track.uri) != "track":
 			continue
-		print track.uri
-		sp.metadata_request("track", SpotifyUtil.uri2id(track.uri), track_metadata_callback)
+		uris.append(track.uri)
+	
+	sp.metadata_request(uris, track_metadata_callback)
 
 def userdata_callback(sp, result):
 	print result["user"]
 
 def login_callback(sp, result):
 	#sp.user_info_request(userdata_callback)
-	#sp.metadata_request("album", SpotifyUtil.uri2id("spotify:album:3OmHoatMS34vM7ZKb4WCY3"), album_metadata_callback)
-	sp.playlist_request("2ITsmcN6qU9NbotiH02Skn", 0, 200, playlist_callback)
+	sp.metadata_request("spotify:album:2mduHypWQwgRXMQ9kEFssu", album_metadata_callback)
+	#sp.metadata_request("spotify:track:5DRLxox45OZGJycLUhJ4h7", track_metadata_callback)
+	#sp.playlist_request("2ITsmcN6qU9NbotiH02Skn", 0, 200, playlist_callback)
 
 sp = SpotifyAPI(login_callback)
 sp.auth()
