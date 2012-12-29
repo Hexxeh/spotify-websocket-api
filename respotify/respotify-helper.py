a3b8e3fc239d5fcba9b2e2eac2d476e5ab7aa348
Liam McLoughlin <hexxeh@hexxeh.net>
2012-12-29 00:26:54 +0000

    Don't recycle session in URI resovler
    After a certain number of tracks in a row Spotify starts ignoring
    us.

--- respotify/respotify-helper.py
+++ respotify/respotify-helper.py
@@ -3,25 +3,29 @@
 import sys; sys.path.append("..")
 from spotify_web.friendly import Spotify
 import cherrypy
+from gevent import wsgi
 
 class SpotifyURIHandler(object):
   def default(self, uri = None):
     if uri == None:
       raise cherrypy.HTTPError(400, "A paramater was expected but not supplied.")
 
+    spotify = Spotify(sys.argv[1], sys.argv[2])
     track = spotify.objectFromURI(uri)
     if track == None:
+      spotify.logout()
       raise cherrypy.HTTPError(404, "Could not find a track with that URI.")
 
     url = track.getFileURL()
     if url == False:
+      spotify.logout()
       raise cherrypy.HTTPError(404, "Could not find a track URL for that URI.")
 
+    spotify.logout()
     raise cherrypy.HTTPRedirect(url)
 
   default.exposed = True
 
-spotify = Spotify(sys.argv[1], sys.argv[2])
 cherrypy.engine.autoreload.unsubscribe()
 cherrypy.config.update({"environment": "production"})
 cherrypy.quickstart(SpotifyURIHandler())
