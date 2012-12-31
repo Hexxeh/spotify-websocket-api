2d6e75ed61eba9f3d2402fdf2aea4885f8f4a544
Liam McLoughlin <hexxeh@hexxeh.net>
2012-12-31 17:58:48 +0000

    Remove apt-get upgrade and make test output verbose

--- install-deps.sh
+++ install-deps.sh
@@ -2,16 +2,10 @@
 
 set -e -x
 VER=$TRAVIS_PYTHON_VERSION
+
 apt-get update -yqq || true
-apt-get upgrade -yqq || true
-apt-get install -q python$VER
+apt-get install -yqq python$VER
 curl -sSLO --retry 5 --fail https://github.com/downloads/denik/packages/python2.7-cython_0.17.1_i386.deb
-#curl -sSLO --retry 5 --fail https://github.com/downloads/denik/packages/python$VER-greenlet_0.4.0_i386.deb
-#curl -sSLO --retry 5 --fail https://github.com/downloads/denik/packages/python$VER-psycopg2_2.4.5_i386.deb
-#curl -sSLO --retry 5 --fail https://github.com/downloads/denik/packages/python$VER-pysendfile_2.0.0_i386.deb
 dpkg -i python2.7-cython_0.17.1_i386.deb
-#dpkg -i python$VER-greenlet_0.4.0_i386.deb
-#dpkg -i python$VER-psycopg2_2.4.5_i386.deb
-#dpkg -i python$VER-pysendfile_2.0.0_i386.deb
 pip install -q requests greenlet protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
 pip install -q git+https://github.com/SiteSupport/gevent.git --upgrade
