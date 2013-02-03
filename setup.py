1e877a9c1abfb5cade24aba4c51adb2f030d8234
Juan Riaza <juanriaza@gmail.com>
2013-02-03 23:17:04 +0100

    setup.py pep8 and requires

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
-import os
 
-setup(name = 'SpotifyWebsocketAPI',
-		version = '0.2',
-		author='Liam McLoughlin',
-		author_email='hexxeh@hexxeh.net',
-		packages=['spotify_web', 'spotify_web.proto'],
-		)
+
+setup(
+    name='SpotifyWebsocketAPI',
+    version='0.2',
+    author='Liam McLoughlin',
+    author_email='hexxeh@hexxeh.net',
+    packages=['spotify_web', 'spotify_web.proto'],
+    requires=[
+        'requests>=1.1.0',
+        'ws4py>=0.2.4',
+        'lxml>=3.1beta1',
+        'protobuf>=2.4.1'],
+)
